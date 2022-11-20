44da56f0aba0963372d0afbc0aabde6de270733e
farhanyousaf786 <farhanyousaf1996@gmail.com>
2022-11-20 16:09:45 -0500

    Edited code flutter v3.0

--- lib/UniversalResources/ConnectionLostPage.dart
+++ lib/UniversalResources/ConnectionLostPage.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 class ConnectionLost extends StatefulWidget {
+  const ConnectionLost({super.key});
+
   @override
   _ConnectionLostState createState() => _ConnectionLostState();
 }
@@ -14,58 +18,57 @@ class _ConnectionLostState extends State<ConnectionLost> {
         elevation: 0.0,
         backgroundColor: Colors.blue,
       ),
-      body: Container(
-        child: Center(
-          child: Column(
-            children: [
-              Padding(
+      body:  Center(
+        child: Column(
+          children: [
+         const   Padding(
+              padding:  EdgeInsets.all(8.0),
+              child: Text(
+                "No Internet",
+                style: TextStyle(
+                    color: Colors.white, fontSize: 20, fontFamily: 'cute'),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                "Make sure that Your WIFI or Mobile Network in On",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.white, fontSize: 10, fontFamily: 'cute'),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
+              },
+              child: Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "No Internet",
-                  style: TextStyle(
-                      color: Colors.white, fontSize: 20, fontFamily: 'cute'),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Icon(
+                      Icons.refresh_sharp,
+                      size: 22,
+                      color: Colors.white,
+                    ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                 const   Text(
+                      "Refresh",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 12,
+                          fontFamily: 'cute'),
+                    ),
+                  ],
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "Make sure that Your WIFI or Mobile Network in On",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      color: Colors.white, fontSize: 10, fontFamily: 'cute'),
-                ),
-              ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamedAndRemoveUntil(context, '/', (_) => false);
-                },
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.refresh_sharp,
-                        size: 22,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Text(
-                        "Refresh",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            fontFamily: 'cute'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
